101e294842d4531634c6d96b4cf0f09013667f94
Amish Ranpariya <amishranpariya@gmail.com>
2026-06-21 19:27:06 +0530

    fix: adds github action deployment pipeline and npm packages upgrade

---
+++ out/_next/static/chunks/app/page-2890656abd434f56.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{3058:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1356,23))}},e=>{e.O(0,[356,441,255,358],()=>e(e.s=3058)),_N_E=e.O()}]);
